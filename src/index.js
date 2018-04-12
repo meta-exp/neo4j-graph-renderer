b1576c41d7a4c2bc87f9bff4c343507bbeecf69b
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2018-04-12 21:16:56 +0200

    Remove comment.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import Neo4jGraphRenderer from 'Neo4jGraphRenderer';
-//onClick={(node)=>console.log(node.propertyMap)}/>
+
 const App = () => (
   <div>
     <Neo4jGraphRenderer url="http://172.20.14.22:7484" user="neo4j" password="neo4j"
                         query="MATCH (n)-[r]->(m) RETURN n,r,m limit 10"
-                        onClickHandler={(node)=>node.propertyMap}/>
+                        onClickHandler={(node)=>console.log(node.propertyMap)}/>
   </div>
 );
 
