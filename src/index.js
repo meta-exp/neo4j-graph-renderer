98af4d69e6fc4c154a2528afeff4fca9c8fcbdaa
Julius Rückin <julius.rueckin@student.hpi.uni-potsdam.de>
2018-04-13 11:40:02 +0200

    Fix onClick Function Naming

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import registerServiceWorker from './registerServiceWorker';
 import Neo4jGraphRenderer from 'Neo4jGraphRenderer';
 
 const App = () => (
-  <div>
-    <Neo4jGraphRenderer url="http://172.20.14.22:7484" user="neo4j" password="neo4j"
-                        query="MATCH (n)-[r]->(m) RETURN n,r,m limit 10"
-                        onClickHandler={(node)=>console.log(node.propertyMap)}/>
-  </div>
+	<div>
+		<Neo4jGraphRenderer url="http://localhost:7494" user="neo4j" password=""
+							query="MATCH (n)-[r]->(m) RETURN n,r,m limit 10"
+							onClick={(node) => console.log(node)} />
+	</div>
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
