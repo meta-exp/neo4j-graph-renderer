77fae19416870cacccc37008e9f2d2ffa02747d5
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2018-04-14 17:48:00 +0200

    revert last commit.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const App = () => (
 	<div>
 		<Neo4jGraphRenderer url="http://localhost:7474" user="neo4j" password="neo4j1"
 							query="MATCH (n)-[r]->(m) RETURN n,r,m limit 10"
-							onClick={node => console.log(node)} />
+							onClick={(event, node) => {console.log(event, node)}} />
 	</div>
 );
 
